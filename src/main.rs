402c27c097030a83a9f441137a9650adc85791a2
myucesan <my96@live.nl>
2021-01-30 09:35:22 +0100

    Reads command with arguments and added poem.txt

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
-use std::io;
-use rand::Rng; // why io::stdin and not RNG::....
-use std::cmp::Ordering;
+use std::env;
+use std::fs;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
-    loop {
-        println!("Please input your guess.");
-        let mut guess = String::new();
+    let query = &args[1];
+    let filename = &args[2];
 
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
+    let contents = fs::read_to_string(filename).expect("Something went wrong while reading the file!");
 
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too Big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-
-
-    }
+    println!("With text:\n{}", contents);
 
 }
